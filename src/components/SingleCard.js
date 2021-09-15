dc182d64aa406973a1f550eac2dda6414b8d0d40
Imad el Ayoubi <imad.ayoubi72@gmail.com>
2021-09-15 21:45:46 +0100

    singleCard component created

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,16 +1,24 @@
 import React from "react";
 
+
+
 const SingleCard = (props) => {
-    // recieving empty array(original state )
+    // recieving empty array(original st
     console.log('props', props.products);
     return (
         <div>
-            <div className="singleCard"  >
-                <h5 className="title">title </h5>
-                <img className='productImage' src='' />
-                <p className="description">describe me </p>
-                <button> Add to Wishlist </button>
-            </div>
+            {props.products.map((element) => {
+                return (
+                    <div className="singleCard"  >
+                        <h5 className="title">{element.name} </h5>
+                        <img className='productImage' src={element.image_link} />
+                        <p className="description">{element.description} </p>
+                        <button> Add to Wishlist </button>
+                    </div>
+
+                )
+            })}
+
 
         </div>
     )
