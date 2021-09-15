2f12521dd4bfe2b34a50d939383df24fd693a33d
Imad el Ayoubi <imad.ayoubi72@gmail.com>
2021-09-15 20:58:19 +0100

    update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import './index.css';
 import $ from 'jquery';
-import styles from './index.css'
 
 class App extends React.Component {
     constructor() {
